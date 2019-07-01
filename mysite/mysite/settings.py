29f15cd10666218a4a4370101307fd20463f2f15
sgaoshang <xxx>
2019-07-01 10:41:56 +0800

    improve, use mariadb

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # SQLite is fine for now
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'polls',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
---
+++ mysite/polls/schema.sql
@@ -0,0 +1,8 @@
+INSERT INTO polls_question
+VALUES 
+  (1, 'sgao handsome?', '2019-06-28 02:47:24.000000');
+
+INSERT INTO polls_choice
+VALUES 
+  (1, 'yes', 0, 1),
+  (2, 'no', 0, 1);
